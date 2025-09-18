ccf93facfa8dafdd144b7505d37ed72f3dd6dd41
shesherao-narote7 <shesheraonarote@cartechindia.com>
2025-09-18 18:11:04 +0530

    Delete src/main/java/com/cartechindia/exception/DuplicateResourceException.java

--- src/main/java/com/cartechindia/exception/DuplicateResourceException.java
+++
@@ -1,8 +0,0 @@
-package com.cartechindia.exception;
-
-// For duplicate values like email/phone
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
