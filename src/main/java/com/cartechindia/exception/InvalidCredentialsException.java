5f0b3447d4dca93ab36b82153ccb3cd675c41da4
shesherao-narote7 <shesheraonarote@cartechindia.com>
2025-09-18 18:11:35 +0530

    Delete src/main/java/com/cartechindia/exception/InvalidCredentialsException.java

--- src/main/java/com/cartechindia/exception/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package com.cartechindia.exception;
-
-// Thrown when username/email/phone + password do not match
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
