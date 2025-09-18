70aabc16609bb8de730f575e488e0d5b405afff8
shesherao-narote7 <shesheraonarote@cartechindia.com>
2025-09-18 18:12:18 +0530

    Delete src/main/java/com/cartechindia/exception/OtpException.java

--- src/main/java/com/cartechindia/exception/OtpException.java
+++
@@ -1,8 +0,0 @@
-package com.cartechindia.exception;
-
-// For OTP issues
-public class OtpException extends RuntimeException {
-    public OtpException(String message) {
-        super(message);
-    }
-}
