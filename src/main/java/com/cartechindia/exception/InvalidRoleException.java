92ed274bd04eae517b68908d30c728bc05d359ea
shesherao-narote7 <shesheraonarote@cartechindia.com>
2025-09-18 18:11:56 +0530

    Delete src/main/java/com/cartechindia/exception/InvalidRoleException.java

--- src/main/java/com/cartechindia/exception/InvalidRoleException.java
+++
@@ -1,8 +0,0 @@
-package com.cartechindia.exception;
-
-// For invalid role (ADMIN, DEALER, etc.)
-public class InvalidRoleException extends RuntimeException {
-    public InvalidRoleException(String message) {
-        super(message);
-    }
-}
