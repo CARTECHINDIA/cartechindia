334b1b38b6567b27feb6d4e190ebaa69d48163ea
Shesherao Narote <shesheraonarote@cartechindia.com>
2025-10-16 17:08:03 +0530

    Updated otp api

--- src/main/java/com/cartechindia/service/OtpService.java
+++ src/main/java/com/cartechindia/service/OtpService.java
@@ -1,7 +1,11 @@
 package com.cartechindia.service;
 
+import com.cartechindia.entity.Otp;
+import com.cartechindia.entity.User;
+
 public interface OtpService {
 
     public String verifyOtp(String email, String otpCode);
     String resendOtp(String email);
+    Otp createOtp(User user);
 }
